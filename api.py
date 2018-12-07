59a06945065b07fceadb206c6ad48debf7222642
injecti0n <rootgebruiker@gmail.com>
2018-12-07 09:23:34 +0100

    matploitlib

--- api.py
+++ api.py
@@ -1,7 +1,7 @@
 import json, ast
 from pprint import pprint
-#import numpy as np
-#import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.pyplot as plt
 import pprint
 print('''
  (   (       )        (     
@@ -29,6 +29,10 @@ def count(list, feedback):
 print("How many SOS: ", count("data.json", "SOS"),"\n")
 print("How many GOOD FEEDBACK: ", count("data.json", "GOOD"),"\n")
 print("How many BAD FEEDBACK: ", count("data.json", "BAD"),"\n")
+sos = count("data.json", "SOS")
+good = count("data.json", "GOOD")
+bad = count("data.json", "BAD")
+
 
 #HowManyRecords = input("How many records would you like to see? ex[1-100] ")
 with open('data.json', 'r') as f:
@@ -51,20 +55,17 @@ for r107s in CleanData:
     ultrasonic = len(r107s['sensors']['ultrasonic_sensor'])
     acce = len(r107s['sensors']['accelerometer'])
     light = len(r107s['sensors']['light_sensor'])
-    #print(r107s['feedback'])
     #str = 'Number of users = {0}'.format(len(r107s))
-
-    #print(r107s['feedback'][0])
-
     print("\n")
-    #print(r107s['sensors']['ultrasonic_sensor'][0])
-    #print(r107s['feedback'][0])
 
-
-
-'''
 # Fake dataset
-height = [r107s['sensors']['ultrasonic_sensor'][0],r107s['sensors']['ultrasonic_sensor'][1],r107s['sensors']['ultrasonic_sensor'][2]]
+
+# Sensors values stats
+#height = [r107s['sensors']['ultrasonic_sensor'][0],r107s['sensors']['ultrasonic_sensor'][1],r107s['sensors']['ultrasonic_sensor'][2]]
+
+# feedback stats
+height = sos,good,bad
+
 bars = ('VALUE1','VALUE2','VALUE3')
 y_pos = np.arange(len(bars))
  
@@ -83,5 +84,4 @@ plt.ylim(0,60)
 plt.xticks(y_pos, bars)
  
 # Show graphic
-#plt.show()
-'''
+plt.show()
